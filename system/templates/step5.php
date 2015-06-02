90e262dcbf216886b8e90cd9230bd9c3073844fc
Volodymyr Flyonts <flyonts@ukr.net>
2015-06-02 04:02:11 +0300

    cosmetic text

--- system/templates/step5.php
+++ system/templates/step5.php
@@ -7,7 +7,7 @@
 <div class="row">
   <div class="col-sm-6">
     <div class="panel panel-default">
-      <div class="panel-heading">Створено електронний документ з обов'язковими реквізитами</div>
+      <div class="panel-heading">Створено електронний документ з наступними реквізитами</div>
       <table class="table">
         <tr><td>Час голосування:</td>
           <td><?= date('Y-m-d H:i:s', $_SESSION['vote_time']); ?></td></tr>
